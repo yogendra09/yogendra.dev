6ad4367cbd6e89792c49ccda5caaeb745a782b01
yogendra09 <yogendrayadav168@gmail.com>
2024-06-27 16:13:06 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -10,17 +10,18 @@ const Loader = () => {
   }, []);
 
   const handleLoad = () => {
+
     // setIsLoading(false);
     gsap.to('.loader',{
-        height:0,
+        height:"0vh",
         ease:"power1.inOut",
         duration:1
     })
   };
 
-  if (isLoading) {
-    return <div className="loader absolute z-10 h-[10vh]  w-full bg-[#121212] flex items-center justify-center overflow-hidden">Loading...</div>;
-  }
+//   if (isLoading) {
+    return <div className="loader absolute z-10 h-[10vh]  w-full bg-[#121212] text-[#f5f5f5]  flex items-center justify-center overflow-hidden">Loading...</div>;
+//   }
 
 };
 
